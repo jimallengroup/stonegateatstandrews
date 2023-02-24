09e6227d08736403d93e97264f594026657119c2
jimallengroup <115572232+jimallengroup@users.noreply.github.com>
2023-02-24 14:47:40 -0500

    add

--- js/custom_i0zj5Ffr.js
+++ js/custom_i0zj5Ffr.js
@@ -1,3 +1,16 @@
+/* Slide 29 (#3) */
+
+$(function(){
+  window.swiper115 = new Swiper('.swiper-115', {
+    slidesPerView: 1,
+    spaceBetween: 25,
+    navigation: {
+      nextEl: '.swiper-115-next',
+      prevEl: '.swiper-115-prev', 
+    },      
+  });
+});
+
 /* Example Contact Form Redirect (#5) */
 
   // This script will redirect your visitors on the successful form submit
